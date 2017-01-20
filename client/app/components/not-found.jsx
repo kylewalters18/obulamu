06aef9d8de44d7d4e4af1ab7af94baf9c213d572
Kyle Walters <kylewalters18@gmail.com>
2017-01-19 20:26:39 -0700

    simple 404 not found page

---
+++ client/app/components/not-found.jsx
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react';
+
+class NotFound extends React.Component {
+
+  render() {
+    return (
+      <h1>404 Not Found</h1>
+    );
+  }
+}
+
+NotFound.propTypes = {};
+
+export default NotFound;
--- client/app/index.jsx
+++ client/app/index.jsx
@@ -2,6 +2,7 @@ import { Route, Router, browserHistory } from 'react-router';
 import { applyMiddleware, createStore } from 'redux';
 
 import AppContainer from 'containers/app-container';
+import NotFound from 'components/not-found';
 import PatientContainer from 'containers/patient-container';
 import { Provider } from 'react-redux';
 import React from 'react';
@@ -16,6 +17,7 @@ render(
     <Router history={browserHistory}>
       <Route path="/" component={AppContainer} />
       <Route path="/patient/:id" component={PatientContainer} />
+      <Route path="*" component={NotFound} />
     </Router>
   </Provider>,
   document.getElementById('app')
