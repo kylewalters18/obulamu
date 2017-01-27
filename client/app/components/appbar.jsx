d8d3be33a659a13cc289b13140393236bcad0066
Kyle Johnson <kyjohnso@gmail.com>
2017-01-27 05:51:44 -0700

    moved appbar to new component, and moved patient information into a react Card component

---
+++ client/app/components/appbar.jsx
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+
+import AppBar from 'material-ui/AppBar';
+
+const Appbar = props => (
+  <div>
+    <AppBar
+      title="Obulamu"
+      iconClassNameRight="muidocs-icon-navigation-expand-more"
+    />
+  </div>
+);
+
+
+export default Appbar;
