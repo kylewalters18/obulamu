699e51320583c260034834a6806d76236db31366
Kyle Johnson <kyjohnso@gmail.com>
2017-02-06 00:36:58 -0700

    WOW - post works out of the box by passing a model key as unicode, when the model itself expects an object, django is the coolestgit add -A

--- server/app/views.py
+++ server/app/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from server.app.serializers import PatientSerializer, NoteSerializer
 from server.app.models import Patient, Note
 
