a819aff27f2627a725e1246216a3c147aec27658
Kyle Johnson <kyjohnso@gmail.com>
2017-02-06 01:38:03 -0700

    corrected single digit lookup fields to be arbitrary length

--- server/urls.py
+++ server/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^patients/$', views.PatientViewSet.as_view({'get': 'list',
                                                    'post': 'create'})),
-    url(r'^patients/(?P<patient>[0-9])/$',views.PatientViewSet.as_view(
+    url(r'^patients/(?P<patient>\d+)/$',views.PatientViewSet.as_view(
                                                   {'get': 'list'})),
-    url(r'^patients/(?P<patient>[0-9])/notes/$', views.NoteViewSet.as_view(
+    url(r'^patients/(?P<patient>\d+)/notes/$', views.NoteViewSet.as_view(
                                                   {'get': 'list',
                                                    'post': 'create'})),
-    url(r'^patients/(?P<patient>[0-9])/notes/(?P<note>[0-9])/$',
+    url(r'^patients/(?P<patient>\d+)/notes/(?P<note>\d+)/$',
                                              views.NoteViewSet.as_view(
                                                   {'get': 'list'})),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
