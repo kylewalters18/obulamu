2c8846c175e3a9ce61b8b6b51e041df9a5a6dd52
schretzilla <schretzilla@gmail.com>
2017-02-14 10:24:40 -0500

    Updated DB settings to point towards PostgresDB

--- server/settings.py
+++ server/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
