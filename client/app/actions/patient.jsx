23dd58c0a5e1a54ce58668eab1151da45a58e67c
Kyle Johnson <kyjohnso@gmail.com>
2017-03-03 21:01:44 -0700

    reset note when patient id, changes

--- client/app/actions/patient.jsx
+++ client/app/actions/patient.jsx
@@ -2,7 +2,19 @@ import axios from 'axios';
 import makeActionCreator from 'actions/util';
 
 export const updatePatientFilter = makeActionCreator('UPDATE_PATIENT_FILTER', 'filter');
-export const updateCurPatient = makeActionCreator('UPDATE_CURRENT_PATIENT', 'curPatient');
+
+export function updateCurPatient(curPatient) {
+  return function (dispatch) {
+    dispatch({
+      type: 'UPDATE_CURRENT_PATIENT',
+      curPatient,
+    });
+    dispatch({ type: 'UPDATE_NEW_NOTE',
+      newNote: '',
+    });
+  };
+}
+
 const receivePatients = makeActionCreator('RECEIVE_PATIENTS', 'patients');
 export const loadPatients = () => (dispatch) => {
     // async call to get the new data
